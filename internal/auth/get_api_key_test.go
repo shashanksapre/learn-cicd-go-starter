7b4095ae15227ea82691fdc51d2babfc0671677f
Shashank Sapre <shashank.sapre@outlook.com>
2026-06-06 15:15:09 +0530

    adding tests

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,37 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestAuthorizationHeader(t *testing.T) {
+	// no authorization header should return error
+	headers := http.Header{}
+	headers.Add("hello", "there")
+	_, err := GetAPIKey(headers)
+
+	if err == nil {
+		t.Errorf("Expected an error for no authorization header")
+	}
+
+	// invalid authorization header should return error
+	headers.Add("Authorization", "bearer token")
+	_, err = GetAPIKey(headers)
+
+	if err == nil {
+		t.Errorf("Expected an error for invalid authorization header")
+	}
+
+	// correct authorization header should not return error
+	headers.Set("Authorization", "ApiKey 123")
+	apiKey, err := GetAPIKey(headers)
+
+	if err != nil {
+		t.Errorf("Expected to pass with correct authorization header")
+	}
+
+	if apiKey != "12" {
+		t.Errorf("Expected ApiKey to be 123, but received %s", apiKey)
+	}
+}
